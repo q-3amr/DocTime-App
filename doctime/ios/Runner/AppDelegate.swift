3a8bf60cf5178d63d336d072da233c7ea67e9c13
Laith Maray <lamarei22@cit.just.edu.jo>
2026-04-01 19:36:26 +0300

    Setup Google Maps config for Android/iOS and create core services folder

--- doctime/ios/Runner/AppDelegate.swift
+++ doctime/ios/Runner/AppDelegate.swift
@@ -1,12 +1,17 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps // 1. تأكد إنك ضفت هاي
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // 2. حط مفتاح الـ iOS هون
+    GMSServices.provideAPIKey("AIzaSyDUaQjJKBcmhN7JEcZbspwfOImC35-bFaw")
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
