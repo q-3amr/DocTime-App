d00c24b286a2714130b9654b76dab7d719e0c55e
AhnafBatayneh <ahnafbatayneh@gmail.com>
2026-06-16 09:24:54 +0300

    comment

--- doctime/lib/models/chat_message.dart
+++ doctime/lib/models/chat_message.dart
@@ -1,20 +1,25 @@
 ﻿import 'package:uuid/uuid.dart';
 
-enum MessageSender { user, bot }
+enum MessageSender { user, bot } // تعريف نوع المرسل، سواء كان المستخدم أو البوت
 
 class ChatMessage {
-  final String id;
-  final String text;
-  final MessageSender sender;
-  final DateTime timestamp;
+  final String id; // معرف فريد لكل رسالة
+  final String text; // نص الرسالة
+  final MessageSender sender; // المرسل (المستخدم أو البوت)
+  final DateTime timestamp; // الطابع الزمني للرسالة
 
   ChatMessage({
-    String? id,
-    required this.text,
-    required this.sender,
-    DateTime? timestamp,
+    // منشئ الكلاس
+    String? id, // معرف اختياري، إذا لم يتم توفيره سيتم توليد معرف فريد
+    required this.text, // نص الرسالة مطلوب
+    required this.sender, // المرسل مطلوب
+    DateTime?
+        timestamp, // الطابع الزمني اختياري، إذا لم يتم توفيره سيتم استخدام الوقت الحالي
   })  : id = id ?? Uuid().v4(),
-        timestamp = timestamp ?? DateTime.now();
+        timestamp = timestamp ??
+            DateTime
+                .now(); // إذا لم يتم توفير الطابع الزمني، سيتم استخدام الوقت الحالي
 
-  bool get isUser => sender == MessageSender.user;
+  bool get isUser =>
+      sender == MessageSender.user; // تحقق مما إذا كانت الرسالة من المستخدم
 }
